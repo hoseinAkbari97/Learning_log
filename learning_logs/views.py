93e44727b2d234687cf74740b7ddc20343e0b1ad
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-06-13 00:44:16 +0330

    Topics page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """The home page for Learning Logs."""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
