aaaed1a2164e6a4fa1ac0ea67b8ff09e4f20b18e
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-06-14 21:37:23 +0330

    Individual topic pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # Home page
     path('', views.index, name='index'),
     # Page that shows all topics.
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
