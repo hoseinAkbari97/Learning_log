57024a1d611e8c7a88e9c50b7a2037702dbd529c
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-06-19 12:33:05 +0330

    Including the URLs from users

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', include('learning_logs.urls')),
 ]
