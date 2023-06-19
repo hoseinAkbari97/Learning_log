1c81fb8eee360fa40cbf01c6250e652ac08526e0
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-06-19 17:03:57 +0330

    The login page

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+"""Define URL patterns for users"""
+
+from django.urls import path, include
+
+app_name = 'users'
+urlpatterns = [
+    # Include default auth urls.
+    path('', include('django.contrib.auth.urls')),
+]
